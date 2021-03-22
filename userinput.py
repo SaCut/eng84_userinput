a12410c03f3d8d1484dd789bc50082fdb43e51d2
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-22 17:16:14 +0000

    added README.md, .gitignore and userinput.py

---
+++ userinput.py
@@ -0,0 +1,22 @@
+# Activity
+
+# Get user input for certain variables and then display the type of those variables
+first_name = input("Please insert your first name: ")
+
+last_name = input("Please insert your last name: ")
+
+age = input("Please insert your age: ")
+
+salary = input("Please insert your salary: ")
+
+date_of_birth = input("Please insert your date of birth: ")
+
+post_code = input("Please insert your post code: ")
+
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(salary))
+print(type(date_of_birth))
+print(type(post_code))
